ad7d85c4e972b3fa99577acdbd3fcfd610f53fe0
Adam Lusted <adam@etoxin.net>
2014-05-24 00:44:52 +1000

    Added radio functionailty

--- assets/js/heartbeat.js
+++ assets/js/heartbeat.js
@@ -1,29 +1,55 @@
-var socket = io.connect('http://localhost');
+angular.module('HeartBeat', ['ui.bootstrap'])
+    .run(function($rootScope){
+        console.log('HeartBeat', $rootScope);
 
-function SynthCtrl($scope){
-    console.log('SynthCtrl', $scope);
-}
-
-    function OscillatorCtrl($scope) {
-        console.log('OscillatorCtrl', $scope);
-    }
-
-
-    function FrequencyCtrl($scope) {
-        console.log('FrequencyCtrl', $scope);
-    }
-
-
-        function NoteCtrl($scope) {
-            console.log('NoteCtrl', $scope);
-        }
+        $rootScope.socket = io.connect('http://localhost');
+    })
 
 
 
+    .controller('SynthCtrl', function($scope, $rootScope) {
+        console.log('SynthCtrl', $scope);
+
+        $rootScope.socket.on('heartbeat', function (data) {
+            console.log(data);
+        });
+    })
 
 
 
+        .controller('OscillatorCtrl', function($scope, $rootScope) {
+            console.log('OscillatorCtrl', $scope);
 
-socket.on('heartbeat', function (data) {
-    console.log(data);
-});
+            $scope.oscillators = [
+                {type: "sine"},
+                {type: "square"},
+                {type: "sawtooth"},
+                {type: "triangle"}
+            ]
+
+            $scope.oscillatorModel = 'sawtooth';
+        })
+
+
+
+        .controller('FrequencyCtrl', function($scope, $rootScope) {
+            console.log('FrequencyCtrl', $scope);
+        })
+
+
+
+            .controller('NoteCtrl', function($scope, $rootScope) {
+                console.log('NoteCtrl', $scope);
+
+                $scope.notes = [
+                    {name: "C", freq: 261.6},
+                    {name: "D", freq: 293.7},
+                    {name: "E", freq: 329.6},
+                    {name: "F", freq: 349.2},
+                    {name: "G", freq: 392.0},
+                    {name: "A", freq: 440.0},
+                    {name: "B", freq: 493.9}
+                ];
+
+                $scope.noteModel = 'F';
+            });
