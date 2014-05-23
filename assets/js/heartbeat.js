99cb56a6205d833028943d2830ff7b0bf7ec6fe7
etoxin <adam@etoxin.net>
2014-05-24 09:54:49 +1000

    change the model up a little

--- assets/js/heartbeat.js
+++ assets/js/heartbeat.js
@@ -2,6 +2,29 @@ angular.module('HeartBeat', ['ui.bootstrap'])
     .run(function($rootScope){
         console.log('HeartBeat', $rootScope);
 
+        $rootScope.synth = {
+            frequency: 440,
+            detune: 0.1,
+            type: 'sawtooth'
+        }
+
+        $rootScope.oscillators = [
+            {type: "sine"},
+            {type: "square"},
+            {type: "sawtooth"},
+            {type: "triangle"}
+        ]
+
+        $rootScope.notes = [
+            {name: "C", freq: 261.6},
+            {name: "D", freq: 293.7},
+            {name: "E", freq: 329.6},
+            {name: "F", freq: 349.2},
+            {name: "G", freq: 392.0},
+            {name: "A", freq: 440.0},
+            {name: "B", freq: 493.9}
+        ];
+
         $rootScope.socket = io.connect('http://localhost');
     })
 
@@ -9,7 +32,6 @@ angular.module('HeartBeat', ['ui.bootstrap'])
 
     .controller('SynthCtrl', function($scope, $rootScope) {
         console.log('SynthCtrl', $scope);
-
         $rootScope.socket.on('heartbeat', function (data) {
             console.log(data);
         });
@@ -19,14 +41,7 @@ angular.module('HeartBeat', ['ui.bootstrap'])
 
         .controller('OscillatorCtrl', function($scope, $rootScope) {
             console.log('OscillatorCtrl', $scope);
-
-            $scope.oscillators = [
-                {type: "sine"},
-                {type: "square"},
-                {type: "sawtooth"},
-                {type: "triangle"}
-            ]
-
+            $scope.oscillators = $rootScope.oscillators;
             $scope.oscillatorModel = 'sawtooth';
         })
 
@@ -34,22 +49,13 @@ angular.module('HeartBeat', ['ui.bootstrap'])
 
         .controller('FrequencyCtrl', function($scope, $rootScope) {
             console.log('FrequencyCtrl', $scope);
+            $scope.frequencyModel = $rootScope.synth.frequency;
         })
 
 
 
             .controller('NoteCtrl', function($scope, $rootScope) {
                 console.log('NoteCtrl', $scope);
-
-                $scope.notes = [
-                    {name: "C", freq: 261.6},
-                    {name: "D", freq: 293.7},
-                    {name: "E", freq: 329.6},
-                    {name: "F", freq: 349.2},
-                    {name: "G", freq: 392.0},
-                    {name: "A", freq: 440.0},
-                    {name: "B", freq: 493.9}
-                ];
-
+                $scope.notes = $rootScope.notes;
                 $scope.noteModel = 'F';
             });
