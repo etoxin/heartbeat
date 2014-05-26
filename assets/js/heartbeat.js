b52a55654248a83ba530f30d4f0e63a559a4922d
etoxin <adam@etoxin.net>
2014-05-26 21:21:46 +1000

    Why won't this work

--- assets/js/heartbeat.js
+++ assets/js/heartbeat.js
@@ -2,19 +2,10 @@ angular.module('HeartBeat', ['ui.bootstrap'])
     .run(function($rootScope){
         console.log('HeartBeat', $rootScope);
 
-        $rootScope.synth = {
-            frequency: 440,
-            detune: 0.1,
-            type: 'sawtooth'
-        }
+        // we'll define a few vars.
+        $rootScope.oscillators = [];
 
-        $rootScope.oscillators = [
-            {type: "sine"},
-            {type: "square"},
-            {type: "sawtooth"},
-            {type: "triangle"}
-        ]
-
+        // Key's to frequencies
         $rootScope.notes = [
             {name: "C", freq: 261.6},
             {name: "D", freq: 293.7},
@@ -24,6 +15,37 @@ angular.module('HeartBeat', ['ui.bootstrap'])
             {name: "A", freq: 440.0},
             {name: "B", freq: 493.9}
         ];
+
+        // properties for the synth.
+        $rootScope.synthProperties = {
+            frequency: 50,
+            amountOfoscillators: 3,
+            detune: 0.01,
+            type: 'sawtooth'
+        }
+
+        var synth = new webkitAudioContext();
+        for (var i = 0; i < $rootScope.synthProperties.amountOfoscillators; i++) {
+            $rootScope.oscillators[i] = synth.createOscillator();
+            $rootScope.oscillators[i].frequency.value = $rootScope.synthProperties.frequency - ($rootScope.synthProperties.detune * i);
+            $rootScope.oscillators[i].type = $rootScope.synthProperties.type;
+            $rootScope.oscillators[i].connect(synth.destination);
+            $rootScope.oscillators[i].start(0);
+        }
+
+
+        // update the oscillators
+        $rootScope.update = {
+            frequency: function () {
+                console.log('boom');
+                $rootScope.oscillators.forEach(function(osc, index){
+                    osc.frequency.value = $rootScope.synthProperties.frequency - ($rootScope.synthProperties.detune * i);
+                    console.log(osc.frequency.value, $rootScope.synthProperties.frequency);
+                })
+
+            }
+        }
+
 
         $rootScope.socket = io.connect('http://localhost');
     })
@@ -41,7 +63,15 @@ angular.module('HeartBeat', ['ui.bootstrap'])
 
         .controller('OscillatorCtrl', function($scope, $rootScope) {
             console.log('OscillatorCtrl', $scope);
-            $scope.oscillators = $rootScope.oscillators;
+
+            // here we have a list of the types of oscillators
+            $scope.oscillators = [
+                {type: "sine"},
+                {type: "square"},
+                {type: "sawtooth"},
+                {type: "triangle"}
+            ]
+            $scope.oscillators = $scope.oscillators;
             $scope.oscillatorModel = 'sawtooth';
         })
 
@@ -49,7 +79,13 @@ angular.module('HeartBeat', ['ui.bootstrap'])
 
         .controller('FrequencyCtrl', function($scope, $rootScope) {
             console.log('FrequencyCtrl', $scope);
-            $scope.frequencyModel = $rootScope.synth.frequency;
+            $scope.frequencyModel = $rootScope.synthProperties.frequency;
+
+            $scope.changeFreq = function () {
+                $rootScope.synthProperties.frequency = $scope.frequencyModel;
+                $rootScope.update.frequency();
+            }
+
         })
 
 
